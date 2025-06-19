7380e2a923999d9b9a32a3190371e6337f23f4d9
danielfhnw <daniel.jakober@fhnw.ch>
2025-06-19 12:51:19 +0200

    asdf

--- Statemachine.py
+++ Statemachine.py
@@ -1,5 +1,7 @@
 import IOs
 
+# ACHTUNG ALLE K9 sind auf K8 gesetzt und K8 auf K7
+
 state = "INIT"
 soll_vend = 0
 
@@ -7,9 +9,9 @@ def get_state():
     global state
 
     if state == "INIT":
-        if IOs.get_button(7):
+        if IOs.get_button(6):
             state = "MANUAL_L"
-        elif IOs.get_button(8):
+        elif IOs.get_button(7):
             state = "CALIB"
 
     elif state == "CALIB":
