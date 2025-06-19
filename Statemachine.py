9e882ffbfeba0829af3d396a84417a0bb5c47834
danielfhnw <daniel.jakober@fhnw.ch>
2025-06-19 13:04:38 +0200

    more states

--- Statemachine.py
+++ Statemachine.py
@@ -9,12 +9,130 @@ def get_state():
     global state
 
     if state == "INIT":
-        if IOs.get_button(6):
+        if IOs.get_button(0):
+            pass
+        elif IOs.get_button(1):
+            pass
+        elif IOs.get_button(2):
+            pass
+        elif IOs.get_button(3):
+            pass
+        elif IOs.get_button(4):
+            pass
+        elif IOs.get_button(5):
+            pass
+        elif IOs.get_button(6):
             state = "MANUAL_L"
         elif IOs.get_button(7):
             state = "CALIB"
 
     elif state == "CALIB":
-        pass
+        if IOs.get_button(0):
+            state = "AUTO"
+        elif IOs.get_button(1):
+            state = "SEMI"
+        elif IOs.get_button(2):
+            state = "MANUAL_L"
+        elif IOs.get_button(3):
+            state = "EDGE_L"
+        elif IOs.get_button(4):
+            pass
+        elif IOs.get_button(5):
+            pass
+        elif IOs.get_button(6):
+            pass
+        elif IOs.get_button(7):
+            pass
+        
+
+    elif state == "MANUAL_L":
+        if IOs.get_button(0):
+            state = "AUTO"
+        elif IOs.get_button(1):
+            state = "SEMI"
+        elif IOs.get_button(2):
+            state = "MANUAL_L"
+        elif IOs.get_button(3):
+            state = "EDGE_L"
+        elif IOs.get_button(4):
+            state = "CALIB"
+        elif IOs.get_button(5):
+            pass
+        elif IOs.get_button(6):
+            pass
+        elif IOs.get_button(7):
+            pass
+
+    elif state == "ERROR":
+        if IOs.get_button(0):
+            state = "AUTO"
+        elif IOs.get_button(1):
+            state = "SEMI"
+        elif IOs.get_button(2):
+            state = "MANUAL_L"
+        elif IOs.get_button(3):
+            state = "EDGE_L"
+        elif IOs.get_button(4):
+            state = "CALIB"
+        elif IOs.get_button(5):
+            pass
+        elif IOs.get_button(6):
+            pass
+        elif IOs.get_button(7):
+            pass
+
+    elif state == "SEMI":
+        if IOs.get_button(0):
+            state = "AUTO"
+        elif IOs.get_button(1):
+            state = "SEMI"
+        elif IOs.get_button(2):
+            state = "MANUAL_L"
+        elif IOs.get_button(3):
+            state = "EDGE_L"
+        elif IOs.get_button(4):
+            state = "CALIB"
+        elif IOs.get_button(5):
+            pass
+        elif IOs.get_button(6):
+            pass
+        elif IOs.get_button(7):
+            pass
+
+    elif state == "AUTO":
+        if IOs.get_button(0):
+            state = "AUTO"
+        elif IOs.get_button(1):
+            state = "SEMI"
+        elif IOs.get_button(2):
+            state = "MANUAL_L"
+        elif IOs.get_button(3):
+            state = "EDGE_L"
+        elif IOs.get_button(4):
+            state = "CALIB"
+        elif IOs.get_button(5):
+            pass
+        elif IOs.get_button(6):
+            pass
+        elif IOs.get_button(7):
+            pass
+
+    elif state == "EDGE_L":
+        if IOs.get_button(0):
+            state = "AUTO"
+        elif IOs.get_button(1):
+            state = "SEMI"
+        elif IOs.get_button(2):
+            state = "MANUAL_L"
+        elif IOs.get_button(3):
+            state = "EDGE_L"
+        elif IOs.get_button(4):
+            state = "CALIB"
+        elif IOs.get_button(5):
+            pass
+        elif IOs.get_button(6):
+            pass
+        elif IOs.get_button(7):
+            pass
 
     return state
