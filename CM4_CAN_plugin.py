17681178871b2f0248782ba24dbfa47bb6bb6658
danielfhnw <daniel.jakober@fhnw.ch>
2025-08-13 16:27:02 +0200

    print i

--- CM4_CAN_plugin.py
+++ CM4_CAN_plugin.py
@@ -64,7 +64,8 @@ if __name__ == "__main__":
                 except Exception as e:
                     print("Fehler beim Schreiben der Geometriedaten:", e)
 
-                for _ in range(10):  # Versuche 10 Mal, eine Nachricht zu empfangen
+                for i in range(10):  # Versuche 10 Mal, eine Nachricht zu empfangen
+                    print(i)
                     try:
                         msg = bus.recv(timeout=0.05)
                         if msg is None:
