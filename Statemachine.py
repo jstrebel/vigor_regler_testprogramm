fe3d19c239236d850de686c3438fd683f898e408
danielfhnw <daniel.jakober@fhnw.ch>
2026-01-27 09:30:14 +0100

    added _P if in EDGE and paused

--- Statemachine.py
+++ Statemachine.py
@@ -376,4 +376,4 @@ def get_state():
         enable_geo = True
         pause_bool = False
         cnt_vend = 0
-    return state
+    return state, pause_bool
--- TestGUI_Raspi.py
+++ TestGUI_Raspi.py
@@ -167,8 +167,8 @@ class SliderApp(QMainWindow):
         RedisAPI.set_value("hmi_pos_l", self.get_str(self.get_pos_prozent(p[0], a[0], inversion[0])) + "%")
         RedisAPI.set_value("hmi_pos_r", self.get_str(self.get_pos_prozent(p[1], a[1], inversion[1])) + "%")
         Statemachine.set_vend_curr(a[0])
-        statemachine_state = Statemachine.get_state()
-        CM4API.send_hb_state(statemachine_state)
+        statemachine_state, paused = Statemachine.get_state()
+        CM4API.send_hb_state(statemachine_state, paused)
         RedisAPI.set_value("hmi_state", statemachine_state)
         RedisAPI.set_value("hmi_vend_soll", Statemachine.get_vend_soll())
         if not ui_control:
